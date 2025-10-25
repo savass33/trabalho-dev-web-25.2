bf2c2c9616fff9bf526c0e51a2b17b9e7f17c0f5
Savas <savas.neto20@gmail.com>
2025-10-25 09:46:46 -0300

    feat: resumo reservas page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import DashboardView from "../views/DashboardView";
 // import NovaReservaView from "../views/NovaReservaView";
 import DetalheEspacoView from "../views/DetalheEspacoView";
+import ResumoReservasView from "../views/ResumoReservasView";
+
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<DashboardView />} />
       <Route path="/espaco/:nome" element={<DetalheEspacoView />} /> {/* 👈 nova rota */}
+      <Route path="/resumo-reservas" element={<ResumoReservasView />} />
+
     </Routes>
   );
 }
